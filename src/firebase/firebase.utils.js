506ca585f80142880935c525b4089810ff33fd1d
brcorbett <corbe009@cougars.csusm.edu>
2021-07-21 23:02:44 -0700

    users can now sign up using an email and passsword, now storing user data into firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,23 @@ const config = {
 	appId: "1:414629839144:web:b9e6a665dedc784b5a501d",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapshot = await userRef.get();
+	if (!snapshot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({ displayName, email, createdAt, ...additionalData });
+		} catch (error) {
+			console.log("Error creating user: ", error.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
